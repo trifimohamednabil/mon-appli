6edf0a0e236f5c370ef149d67154d79db37b9ace
trifimohamednabil <mtrifi@ulb.ac.be>
2017-12-18 20:55:09 +0100

    Initialise le projet Maven

---
+++ src/main/java/com/multi.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com;
+
+/**
+ *
+ * @author Nabil
+ */
+public class multi {
+    
+    private int num1;
+    
+    
+    public multi()
+    {}
+    
+    public void multiplication(int nb1)
+    {
+    for(int i=0;i<=10;i++)
+        {
+          num1=nb1*i;
+          System.out.println(num1);
+        }
+    }
+}
---
+++ src/test/java/com/TestMulti.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com;
+
+/**
+ *
+ * @author Nabil
+ */
+public class TestMulti {
+     public static void main( String[] args )
+    {
+        multi x= new multi();
+        x.multiplication(60);
+    }
+    
+}
